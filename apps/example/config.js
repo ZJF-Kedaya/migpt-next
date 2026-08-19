65bc45834e7eaaa132b1b8fc6aaa15c032071469
Zhang JiaFu <111995151+ZJF-Kedaya@users.noreply.github.com>
2026-08-19 14:14:09 +0800

    Update config.js
    修改账号信息

--- apps/example/config.js
+++ apps/example/config.js
@@ -9,19 +9,19 @@ export default {
      *
      * 如果提示找不到设备，请打开调试模式获取设备真实的 name、miotDID 或 mac 地址填入
      */
-    did: 'Xiaomi 智能音箱 Pro',
+    did: '小爱音箱Play增强版',
     /**
      * 小米 ID（一串数字）
      *
      * 注意：不是手机号或邮箱，请在小米账号「个人信息」-「小米 ID」查看
      */
-    userId: '1234567',
+    userId: '1430167450',
     /**
      * 小米账号登录密码
      *
      * 如果提示登录失败，请使用 passToken 登录
      */
-    password: 'xxxxx',
+    password: 'zjf942ljj',
     /**
      * （可选）小米账号 passToken
      *
@@ -40,15 +40,15 @@ export default {
      * - ❌ https://api.openai.com/v1/（最后多了一个 /
      * - ❌ https://api.openai.com/v1/chat/completions（不需要加 /chat/completions）
      */
-    baseURL: 'https://api.openai.com/v1',
+    baseURL: 'https://token.sensenova.cn/v1',
     /**
      * API 密钥
      */
-    apiKey: 'sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    apiKey: 'sk-llMNPRTuZK7Ft13kcpK7UavL1CwwwgGK',
     /**
      * 模型名称
      */
-    model: 'gpt-4.1-mini',
+    model: 'deepseek-v4-flash',
   },
   prompt: {
     /**
